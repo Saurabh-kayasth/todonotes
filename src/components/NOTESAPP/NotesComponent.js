d33a3d3434f4a285899987b62263e14381d6c573
saurabh-kayasth <i.am.saurabh.kayasth@gmail.com>
2020-11-10 11:53:37 +0530

    added long press event in NOTESAPP

--- src/components/NOTESAPP/NotesComponent.js
+++ src/components/NOTESAPP/NotesComponent.js
@@ -1,26 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, Text} from 'react-native';
-import {Surface, useTheme} from 'react-native-paper';
+import {Surface, useTheme, TouchableRipple} from 'react-native-paper';
 
 const NotesComponent = (props) => {
   const {colors} = useTheme();
+  const [pressed, setPressed] = useState(false);
   const text =
     "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+  const handleLongPress = () => {
+    console.log('=========== LONG PRESS ===========');
+    setPressed(!pressed);
+    if (!pressed) {
+      console.log('=========== LONG TRUE ===========');
+      props.handleLongPress(props.item.id);
+    } else {
+      console.log('=========== LONG FALSE ===========');
+      props.handleLongPress(props.item.id);
+    }
+  };
+  const handlePress = () => {
+    // props.handleLongPress(props.item.id);
+    if (pressed) {
+      setPressed(!pressed);
+      props.handleLongPress(props.item.id);
+    }
+    if (props.init) {
+      setPressed(!pressed);
+      props.handleLongPress(props.item.id);
+    }
+  };
+
+  const longPressOut = () => {
+    // props.handleLongPress(props.item.id);
+    // console.log('========= OUTT =============');
+  };
+
   return (
     <Surface
       style={[
         styles.notesContainer,
         {
-          backgroundColor: colors.SecondaryColor,
+          backgroundColor: pressed ? '#595959' : colors.SecondaryColor,
           width: props.orientation === 'LANDSCAPE' ? '23%' : '46%',
         },
       ]}>
-      <Text style={[styles.heading, {color: colors.text}]} numberOfLines={1}>
-        This is dubug time now
-      </Text>
-      <Text style={styles.description} numberOfLines={9}>
-        {text}
-      </Text>
+      <TouchableRipple
+        style={styles.btnContainer}
+        delayLongPress={300}
+        onPress={handlePress}
+        onLongPress={handleLongPress}
+        onPressOut={longPressOut}>
+        <>
+          <Text
+            style={[styles.heading, {color: colors.text}]}
+            numberOfLines={1}>
+            This is dubug time now
+          </Text>
+          <Text style={styles.description} numberOfLines={5}>
+            {text}
+          </Text>
+        </>
+      </TouchableRipple>
     </Surface>
   );
 };
@@ -30,12 +71,15 @@ export default NotesComponent;
 const styles = StyleSheet.create({
   notesContainer: {
     // width: '47%',
-    height: 200,
+    // height: 200,
     elevation: 0,
     margin: 8,
     borderRadius: 10,
+    flexGrow: 0,
+  },
+  btnContainer: {
     padding: 10,
-    paddingTop: 5,
+    // paddingTop: 5,
   },
   heading: {
     // fontWeight: 'bold',
--- src/screens/Notes.js
+++ src/screens/Notes.js
@@ -1,14 +1,20 @@
 import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, useWindowDimensions} from 'react-native';
+import {View, StyleSheet, useWindowDimensions, Text} from 'react-native';
 import {FlatList} from 'react-native-gesture-handler';
-import {useTheme, FAB} from 'react-native-paper';
+import {useTheme, FAB, TouchableRipple} from 'react-native-paper';
 import NotesComponent from '../components/NOTESAPP/NotesComponent';
 import {Styles} from '../styles/Styles';
+import * as Animatable from 'react-native-animatable';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Notes = () => {
   const {colors} = useTheme();
   const window = useWindowDimensions();
   const [orientation, setOrientation] = useState('');
+  const [idList, setIdList] = useState([]);
+  const [init, setInit] = useState(false);
+  const [isFav, setIsFav] = useState(false);
+
   const getOrientation = () => {
     if (window.height < window.width) {
       setOrientation('LANDSCAPE');
@@ -22,6 +28,32 @@ const Notes = () => {
     getOrientation();
   });
 
+  const handleLongPress = (notesId) => {
+    const arr = [...idList];
+    let index = arr.indexOf(notesId);
+    console.log(index);
+    if (index !== -1) {
+      arr.splice(index, 1);
+    } else {
+      arr.push(notesId);
+    }
+    if (arr.length > 0) {
+      setInit(true);
+    } else {
+      setInit(false);
+    }
+    console.log(arr);
+    setIdList(arr);
+  };
+
+  const handleStar = () => {
+    setIsFav(!isFav);
+  };
+
+  const handleDelete = () => {
+    //
+  };
+
   return (
     <View style={[Styles.container, {backgroundColor: colors.BackgroundColor}]}>
       <FlatList
@@ -30,10 +62,34 @@ const Notes = () => {
         contentContainerStyle={{
           justifyContent: 'center',
           padding: 7,
+          flexWrap: 'nowrap',
+          flexGrow: 0,
         }}
-        data={[1, 2, 3, 4, 5, 6, 7, 8]}
+        data={[
+          {id: 1},
+          {id: 2},
+          {id: 3},
+          {id: 4},
+          {id: 5},
+          {id: 6},
+          {id: 7},
+          {id: 8},
+          {id: 9},
+          {id: 10},
+          {id: 11},
+          {id: 12},
+        ]}
         renderItem={({item, index}) => {
-          return <NotesComponent orientation={orientation} />;
+          return (
+            <NotesComponent
+              item={item}
+              index={index}
+              init={init}
+              orientation={orientation}
+              key={index}
+              handleLongPress={handleLongPress}
+            />
+          );
         }}
       />
 
@@ -43,6 +99,27 @@ const Notes = () => {
         icon="plus"
         // onPress={() => addTask()}
       />
+      {idList.length > 0 && (
+        <Animatable.View
+          animation="fadeInUpBig"
+          style={[
+            styles.footer,
+            {
+              backgroundColor: colors.SecondaryColor,
+            },
+          ]}>
+          <TouchableRipple onPress={handleStar}>
+            <Icon
+              name={isFav ? 'star' : 'star-outline'}
+              color="gold"
+              size={30}
+            />
+          </TouchableRipple>
+          <TouchableRipple onPress={handleDelete}>
+            <Icon name="delete-outline" color="#ff0000" size={30} />
+          </TouchableRipple>
+        </Animatable.View>
+      )}
     </View>
   );
 };
@@ -58,6 +135,18 @@ const styles = StyleSheet.create({
     height: 50,
     width: 50,
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  footer: {
+    width: '40%',
+    height: 50,
+    borderRadius: 30,
+    marginBottom: 20,
+    zIndex: 100,
+    padding: 10,
+    alignSelf: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
   },
 });
